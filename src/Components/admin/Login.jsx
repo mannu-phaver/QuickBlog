9c6ba2ac1e9316cd6dd38c35acf9aa65f7c71b7b
manu <somamanojshm.22@gmail.com>
2025-07-04 08:08:28 +0530

    Enter Email and Password Added to admin page

--- src/Components/admin/Login.jsx
+++ src/Components/admin/Login.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 
 const Login = () => {
+  const handleSubmit={
+
+  }
   return (
       <div className='flex items-center justify-center h-screen'>
       <div className='w-full max-w-sm p-6 max-md:m-6 border border-primary/30 shadow-xl shadow-primary/15 rounded-lg'>
@@ -11,6 +14,21 @@ const Login = () => {
             </h1>
             <p className='font-light'>Enter your credentials to access the admin panel</p>
           </div>
+          <form onSubmit={handleSubmit} className='mt-6 w-full sm:max-w-md text-gray-600'>
+          <div className='flex flex-col'>
+          <label> Email </label>
+          <input type="email" required placeholder='your email id'
+          className='border-b-2 border-gray-300 p-2 outline-none mb-6'/>
+          </div>
+          <div className='flex flex-col'>
+          <label> Password </label>
+          <input type="password" required placeholder='your password'
+          className='border-b-2 border-gray-300 p-2 outline-none mb-6'/>
+          </div>
+          <button type="submit" className='w-full py-3 font-medium bg-primary
+          text-white rounded cursor-pointer hover:bg-primary/90 
+          transition-all'> Login </button>
+          </form>
         </div>
       </div>
     </div>

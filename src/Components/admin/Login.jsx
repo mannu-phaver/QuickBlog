cbab199e860b97029ac8208d52dc1d7eb845df19
manu <somamanojshm.22@gmail.com>
2025-07-07 08:23:36 +0530

    Added Use state functionality

--- src/Components/admin/Login.jsx
+++ src/Components/admin/Login.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
-  const handleSubmit={
-
+  const [email,setEmail]=useState('')
+  const [password,setPassword]=useState('')
+  const handleSubmit= async(e)=>{
+      e.preventDefault()
   }
   return (
       <div className='flex items-center justify-center h-screen'>
@@ -17,12 +19,12 @@ const Login = () => {
           <form onSubmit={handleSubmit} className='mt-6 w-full sm:max-w-md text-gray-600'>
           <div className='flex flex-col'>
           <label> Email </label>
-          <input type="email" required placeholder='your email id'
+          <input onChange={e=>setEmail(e.target.value)} value={{email}} type="email" required placeholder='your email id'
           className='border-b-2 border-gray-300 p-2 outline-none mb-6'/>
           </div>
           <div className='flex flex-col'>
           <label> Password </label>
-          <input type="password" required placeholder='your password'
+          <input onChange={e=>setPassword(e.target.value)} value={{password}}type="password" required placeholder='your password'
           className='border-b-2 border-gray-300 p-2 outline-none mb-6'/>
           </div>
           <button type="submit" className='w-full py-3 font-medium bg-primary
